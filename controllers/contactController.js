f7bfb3a5d424fb1803a6d83fbbb51b8077a85bb3
jenny-hub90 <jumbledjenny19@gmail.com>
2023-05-17 19:10:42 +0545

    added user authentication and authorization

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,15 +1,17 @@
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 //@desc for get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 
 const getContacts = asyncHandler(async (req, res) => {
-    res.status(200).json({message:"Get all contacts"});
+    const contacts = await Contact.find({ user_id: req.user.id });
+    res.status(200).json(contacts);
 });
 
 //@desc Create New contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 
 const createContact = asyncHandler(async (req,res)=>{
     console.log("The request body is",req.body);
@@ -18,30 +20,69 @@ const createContact = asyncHandler(async (req,res)=>{
         res.status(400);
         throw new Error("All fields are mandatory")
     }
-    res.status(201).json({message:"Create Contact"});
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+        user_id: req.user.id
+    });
+    
+    res.status(201).json(contact);
 });
 
 //@desc Get contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 
 const getContact = asyncHandler(async (req,res)=>{
-    res.status(200).json({message:`Get contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
 });
 
 //@desc update contacts
 //@route PUT /api/contacts
-//@access public
+//@access private
 
 const updateContact = asyncHandler(async (req,res)=>{
-    res.status(200).json({message:`Update contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403);
+        throw new Error("User don;t have permission to update other user contacts");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true}
+    );
+
+    res.status(200).json(updatedContact);
 });
 
 //@desc delete contacts
 //@route DELETE/api/contacts
-//@access public
+//@access private
 const deleteContact = asyncHandler(async  (req,res)=>{
-    res.status(200).json({message:`Delete contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    if(contact.user_id.toString() !== req.user.id){
+        res.status(403);
+        throw new Error("User don't have permission to update oher contacts");
+    }
+    await Contact.remove();
+    res.status(200).json(contact);
 });
 
 module.exports= { 
--- models/contactModel.js
+++ models/contactModel.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const contactSchema = mongoose.Schema({
+    user_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
     name:{
         type: String,
         required: [true, "Please add the contact name"],
@@ -8,6 +13,13 @@ const contactSchema = mongoose.Schema({
     email:{
         type: String,
         required: [true, "Please add the contact email address"],
-    }
-
+    },
+    phone:{
+        type: String,
+        required: [true, "Please add the contact phone number"],
+    },
+},{
+    timestamps:true,
 });
+
+module.exports = mongoose.model("Contact",contactSchema);
